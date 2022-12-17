49ff107213e3341a65bedde5ddfc347e229fb1c7
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2022-12-16 20:06:25 -0500

    🐛ok fine use `toBitMask`

--- src/test/scala/org/lemon/advent/year2022/Day16Test.scala
+++ src/test/scala/org/lemon/advent/year2022/Day16Test.scala
@@ -22,7 +22,7 @@ class Day16Test extends UnitTest {
       flowValves: Seq[Valve],
       memory: mutable.Map[String, Int] = mutable.Map()
   ): Int =
-    val key = s"$turn#$currentValve#$time#${openFlowValves.hashCode}"
+    val key = s"$turn#$currentValve#$time#${openFlowValves.toBitMask(0)}"
 
     memory.get(key) match
       case Some(memorized) => memorized
