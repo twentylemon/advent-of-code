55b10c6afb946dbfd86daa33b52049cb32e9e25e
Taras Mychaskiw <tarasmy@amazon.com>
2022-12-20 15:04:07 -0500

    🧪 ignore slow day 19 test

--- src/test/scala/org/lemon/advent/year2022/Day19PosthumousTest.scala
+++ src/test/scala/org/lemon/advent/year2022/Day19PosthumousTest.scala
@@ -116,7 +116,7 @@ class Day19PosthumousTest extends UnitTest {
   }
 
   // is quite a slow boy -- slower than the real deal. /shrug
-  test("part 2 example") {
+  ignore("part 2 example") {
     val in = """|Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
                 |Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.""".stripMargin
 
