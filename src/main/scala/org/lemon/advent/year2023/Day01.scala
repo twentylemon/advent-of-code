94cad551cb5b394e9e705f9180c528a132382ff8
twentylemon <taras.mychaskiw@gmail.com>
2023-12-01 00:55:44 -0500

    :sparkle: day 1

--- src/main/scala/org/lemon/advent/year2023/Day01.scala
+++ src/main/scala/org/lemon/advent/year2023/Day01.scala
@@ -1,4 +1,28 @@
 package org.lemon.advent.year2023
 
 private object Day01:
-  def part1() = ()
+  def parse(input: String) = input.linesIterator
+
+  def part1(input: String) = input.linesIterator.map(_.filter(_.isDigit)).map(n => s"${n.head}${n.takeRight(1)}").map(_.toInt).sum
+
+  val nums = Map(
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9,
+  ) ++ (1 to 9).map(x => (x.toString, x)).toMap
+
+  def find(line: String): (Int, Int) =
+    val min = nums.map((k, v) => (line.indexOf(k), k)).filterKeys(_ != -1)
+    val max = nums.map((k, v) => (line.lastIndexOf(k), k)).filterKeys(_ != -1)
+    (nums(min(min.keySet.min)), nums(max(max.keySet.max)))
+
+  def part2(input: String) = input.linesIterator
+    .map(find)
+    .map((x, y) => 10*x + y)
+    .sum
--- src/test/scala/org/lemon/advent/year2023/Day01Test.scala
+++ src/test/scala/org/lemon/advent/year2023/Day01Test.scala
@@ -4,7 +4,30 @@ import org.lemon.advent._
 import org.lemon.advent.year2023.Day01._
 
 class Day01Test extends UnitTest:
-    
+
   test("part 1 example") {
-    part1()
+    val in = """|1abc2
+                |pqr3stu8vwx
+                |a1b2c3d4e5f
+                |treb7uchet""".stripMargin
+    part1(in) shouldBe 142
+  }
+
+  test("part 1") {
+    part1(read(file(2023)(1))) shouldBe 54081
+  }
+
+  test("part 2 example") {
+    val in = """|two1nine
+                |eightwothree
+                |abcone2threexyz
+                |xtwone3four
+                |4nineeightseven2
+                |zoneight234
+                |7pqrstsixteen""".stripMargin
+    part2(in) shouldBe 281
+  }
+
+  test("part 2") {
+    part2(read(file(2023)(1))) shouldBe 54649
   }
