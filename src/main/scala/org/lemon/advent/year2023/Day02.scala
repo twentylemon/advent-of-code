0ffbf5d1f605248d0267335a3dccac4735a1b6a7
twentylemon <taras.mychaskiw@gmail.com>
2023-12-02 09:16:03 -0500

    :sparkle: day 2

---
+++ src/main/scala/org/lemon/advent/year2023/Day02.scala
@@ -0,0 +1,43 @@
+package org.lemon.advent.year2023
+
+private object Day02:
+
+  case class Dice(num: Int, colour: String)
+  case class Game(id: Int, pulls: Seq[Seq[Dice]])
+
+  def parsePulls(pulls: String): Seq[Seq[Dice]] = pulls.split(";")
+    .map(_.split(","))
+    .map(_.map(x => x.strip))
+    .map(_.map(dice => dice match
+      case s"$num $colour" => Dice(num.toInt, colour)
+    ))
+    .map(_.toSeq)
+    .toSeq
+
+  def parse(input: String): Seq[Game] = input.linesIterator
+    .map(line => line match
+      case s"Game $id: $games" => Game(id.toInt, parsePulls(games))
+    )
+    .toSeq
+
+  def getMaxPerColour(game: Game) = game.pulls
+    .flatten
+    .groupBy(_.colour)
+    .mapValues(_.maxBy(dice => dice.num))
+    .mapValues(_.num)
+
+  def isPossible(game: Game): Boolean =
+    val max = getMaxPerColour(game)
+    max("red") <= 12 && max("green") <= 13 && max("blue") <= 14
+
+  def part1(input: String) = parse(input)
+    .filter(isPossible)
+    .map(_.id)
+    .sum
+
+  def part2(input: String) = parse(input)
+    .map(getMaxPerColour)
+    .map(_.values)
+    .map(_.product)
+    .sum
+  
---
+++ src/test/scala/org/lemon/advent/year2023/Day02Test.scala
@@ -0,0 +1,32 @@
+package org.lemon.advent.year2023
+
+import org.lemon.advent._
+import org.lemon.advent.year2023.Day02._
+
+class Day02Test extends UnitTest:
+
+  test("part 1 example") {
+    val in = """|Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".stripMargin
+    part1(in) shouldBe 8
+  }
+
+  test("part 1") {
+    part1(read(file(2023)(2))) shouldBe 2449
+  }
+
+  test("part 2 example") {
+    val in = """|Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".stripMargin
+    part2(in) shouldBe 2286
+  }
+
+  test("part 2") {
+    part2(read(file(2023)(2))) shouldBe 63981
+  }
