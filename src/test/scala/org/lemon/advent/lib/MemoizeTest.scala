be12a842a936f06dbc6c4b67d980a2e029aba73a
twentylemon <taras.mychaskiw@gmail.com>
2023-12-12 15:02:37 -0500

    ✅ simple memoize test

---
+++ src/test/scala/org/lemon/advent/lib/MemoizeTest.scala
@@ -0,0 +1,20 @@
+package org.lemon.advent.lib
+
+import org.lemon.advent.UnitTest
+
+class MemoizeTest extends UnitTest:
+  
+  test("sanity: no memorize does not cache repeated calls") {
+    var x = 0
+    val f: Int => Int = _ => { x = x + 1; x }
+    f(0) shouldBe 1
+    f(0) shouldBe 2
+  }
+
+  test("memoize caches repeated calls") {
+    var x = 0
+    val f: Int => Int = memoize:
+      _ => { x = x + 1; x }
+    f(0) shouldBe 1
+    f(0) shouldBe 1
+  }
