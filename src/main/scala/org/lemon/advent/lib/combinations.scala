db449d12104131bab16500c45a904db4788cd10a
twentylemon <taras.mychaskiw@gmail.com>
2023-12-12 21:46:48 -0500

    :sparkle: add triples helper

--- src/main/scala/org/lemon/advent/lib/combinations.scala
+++ src/main/scala/org/lemon/advent/lib/combinations.scala
@@ -8,3 +8,12 @@ extension [T](xs: Iterable[T])
       (y, j) <- xs.iterator.zipWithIndex
       if i < j
     yield (x, y)
+
+  def triples: Iterator[(T, T, T)] =
+    for
+      (x, i) <- xs.iterator.zipWithIndex
+      (y, j) <- xs.iterator.zipWithIndex
+      (z, k) <- xs.iterator.zipWithIndex
+      if i < j
+      if j < k
+    yield (x, y, z)
--- src/test/scala/org/lemon/advent/lib/CombinationsTest.scala
+++ src/test/scala/org/lemon/advent/lib/CombinationsTest.scala
@@ -2,14 +2,30 @@ package org.lemon.advent.lib
 
 import org.scalacheck._
 import org.lemon.advent._
-import org.lemon.advent.lib.pairs
+import org.lemon.advent.lib.{pairs, triples}
 
 class CombinationsTest extends UnitTest:
 
-  test("number of pairs equal to n choose 2") {
-    check((xs: Seq[Int]) => xs.pairs.length == xs.size * (xs.size - 1) / 2)
+  test("pairs is equal to stdlib combinations(2) of a set") {
+    check((xs: Set[Int]) =>
+      val pair = xs.pairs.toSeq.sorted
+      val combo = xs.toSeq
+        .combinations(2)
+        .map { case Seq(a, b) => (a, b) }
+        .toSeq
+        .sorted
+      pair == combo
+    )
   }
 
-  test("elements from pairs are in collection") {
-    check((xs: Seq[Int]) => xs.pairs.forall((a, b) => xs.contains(a) && xs.contains(b)))
+  test("triples is equal to stdlib combinations(3) of a set") {
+    check((xs: Set[Int]) =>
+      val trip = xs.triples.toSeq.sorted
+      val combo = xs.toSeq
+        .combinations(3)
+        .map { case Seq(a, b, c) => (a, b, c) }
+        .toSeq
+        .sorted
+      trip == combo
+    )
   }
