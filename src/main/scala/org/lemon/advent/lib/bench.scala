33fc4b009c049876465c3512bd163f0a132df719
twentylemon <taras.mychaskiw@gmail.com>
2023-12-16 20:01:08 -0500

    :zap: benchmark stub

--- src/main/scala/org/lemon/advent/lib/bench.scala
+++ src/main/scala/org/lemon/advent/lib/bench.scala
@@ -1,11 +1,16 @@
 package org.lemon.advent.lib
 
+import org.lemon.advent._
+import org.lemon.advent.lib._
 import org.openjdk.jmh.annotations.Benchmark
 
 class Bench:
 
   @Benchmark // Jmh/run org.lemon.advent.lib.Bench
   def bench(): Unit =
-    import org.lemon.advent._
     // import org.lemon.advent.year2023.Day16._
     // part1(read(file(2023)(16)))
+    
+    Area(0 to 100, 0 to 100).toSeq
+
+    ()
