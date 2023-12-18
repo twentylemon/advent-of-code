610577a15aaa9acf47ddef580e34a9a247e4b333
twentylemon <taras.mychaskiw@gmail.com>
2023-12-18 17:12:18 -0500

    :art: remove edge class

--- src/main/scala/org/lemon/advent/year2023/Day18.scala
+++ src/main/scala/org/lemon/advent/year2023/Day18.scala
@@ -7,25 +7,23 @@ import scala.collection.mutable
 
 private object Day18:
 
-  case class Edge(direction: Direction, length: Int)
-
   def parse(input: String) = input.linesIterator
     .map(_ match
-      case s"$dir $len ($_)" => Edge(direction = Direction(dir), length = len.toInt)
+      case s"$dir $len ($_)" => Direction(dir).unitVector * len.toInt
     )
     .toSeq
 
   def parseHex(input: String) = input.linesIterator
     .map(_ match
-      case s"$_ $_ (#${len}0)" => Edge(direction = Direction.Right, length = java.lang.Integer.parseInt(len, 16))
-      case s"$_ $_ (#${len}1)" => Edge(direction = Direction.Down, length = java.lang.Integer.parseInt(len, 16))
-      case s"$_ $_ (#${len}2)" => Edge(direction = Direction.Left, length = java.lang.Integer.parseInt(len, 16))
-      case s"$_ $_ (#${len}3)" => Edge(direction = Direction.Up, length = java.lang.Integer.parseInt(len, 16))
+      case s"$_ $_ (#${len}0)" => Direction.Right.unitVector * java.lang.Integer.parseInt(len, 16)
+      case s"$_ $_ (#${len}1)" => Direction.Down.unitVector * java.lang.Integer.parseInt(len, 16)
+      case s"$_ $_ (#${len}2)" => Direction.Left.unitVector * java.lang.Integer.parseInt(len, 16)
+      case s"$_ $_ (#${len}3)" => Direction.Up.unitVector * java.lang.Integer.parseInt(len, 16)
     )
     .toSeq
 
-  def trace(edges: Seq[Edge]) = edges
-    .foldLeft(List(origin))((list, edge) => list.head.shift(edge.direction, edge.length) :: list)
+  def trace(edges: Seq[Coord]) = edges
+    .foldLeft(List(origin))((list, edge) => list.head + edge :: list)
 
   def innerArea(corners: Seq[Coord]) =
     corners.zip(corners.tail)
@@ -33,11 +31,11 @@ private object Day18:
       .sum
       .abs / 2
 
-  def solve(prs: String => Seq[Edge])(input: String) =
+  def solve(prs: String => Seq[Coord])(input: String) =
     val edges = prs(input)
     val corners = trace(edges)
-    innerArea(corners) + (edges.map(_.length).sum / 2 + 1)
+    innerArea(corners) + (edges.map(_ manhattan origin).sum / 2 + 1)
 
   def part1 = solve(parse)
-  
+
   def part2 = solve(parseHex)
