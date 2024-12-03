c2fdec97344c66199d76c4bc10ebf3a3de4f4002
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2024-12-03 10:22:21 -0500

    ✨ day 3

---
+++ src/main/scala/org/lemon/advent/year2024/Day03.scala
@@ -0,0 +1,20 @@
+package org.lemon.advent.year2024
+
+private object Day03:
+
+  def part1(input: String) = raw"mul\((\d{1,3}),(\d{1,3})\)".r
+    .findAllMatchIn(input)
+    .map(m => m.group(1).toInt * m.group(2).toInt)
+    .sum
+
+  def part2(input: String) =
+    val matches = raw"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))".r
+      .findAllIn(input)
+      .toSeq
+    matches.zipWithIndex
+      .filter(_._1.startsWith("mul"))
+      .filter((_, i) => matches.lastIndexOf("do()", i) >= matches.lastIndexOf("don't()", i))
+      .map(_._1 match
+        case s"mul($a,$b)" => a.toInt * b.toInt
+      )
+      .sum
---
+++ src/test/scala/org/lemon/advent/year2024/Day03Test.scala
@@ -0,0 +1,24 @@
+package org.lemon.advent.year2024
+
+import org.lemon.advent._
+import org.lemon.advent.year2024.Day03._
+
+class Day03Test extends UnitTest:
+
+  test("part 1 example") {
+    val in = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    part1(in) shouldBe 161
+  }
+
+  test("part 1") {
+    part1(read(file(2024)(3))) shouldBe 167650499
+  }
+
+  test("part 2 example") {
+    val in = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    part2(in) shouldBe 48
+  }
+
+  test("part 2") {
+    part2(read(file(2024)(3))) shouldBe 95846796
+  }
