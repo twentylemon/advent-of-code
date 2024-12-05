7a354d494093fd5af8a024334c379b624a816ab7
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2024-12-05 09:59:47 -0500

    ✨ day 5

---
+++ src/main/scala/org/lemon/advent/year2024/Day05.scala
@@ -0,0 +1,34 @@
+package org.lemon.advent.year2024
+
+import org.lemon.advent.lib.`2d`._
+
+private object Day05:
+
+  case class Rule(before: Int, after: Int)
+
+  def parse(input: String) =
+    val Array(rules, pages) = input.split("\n\n")
+    val r = rules.linesIterator.map { case s"$before|$after" => Rule(before.toInt, after.toInt) }.toSeq
+    val p = pages.linesIterator.map(_.split(",").map(_.toInt).toSeq).toSeq
+    (r, p)
+
+  @annotation.tailrec
+  def isOrdered(pages: Seq[Int], rules: Seq[Rule]): Boolean =
+    if rules.isEmpty then true
+    else
+      val rule = rules.head
+      val before = pages.indexOf(rule.before)
+      val after = pages.indexOf(rule.after)
+      if before < 0 || after < 0 || before < after then isOrdered(pages, rules.tail)
+      else false
+
+  def part1(input: String) =
+    val (rules, pages) = parse(input)
+    pages.filter(isOrdered(_, rules)).map(p => p(p.size / 2)).sum
+
+  def fixOrder(pages: Seq[Int], rules: Seq[Rule]): Seq[Int] =
+    pages.sortWith((a, b) => rules.exists(r => r.before == a && r.after == b))
+
+  def part2(input: String) =
+    val (rules, pages) = parse(input)
+    pages.filterNot(isOrdered(_, rules)).map(fixOrder(_, rules)).map(p => p(p.size / 2)).sum
