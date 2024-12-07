25c331c03691ad1c2a8498d9e84080cea74180cb
twentylemon <taras.mychaskiw@gmail.com>
2024-12-07 00:18:16 -0500

    ✨ day 7

---
+++ src/main/scala/org/lemon/advent/year2024/Day07.scala
@@ -0,0 +1,27 @@
+package org.lemon.advent.year2024
+
+import cats.kernel.Eq
+
+private object Day07:
+
+  case class Equation(result: Long, operands: Seq[Long])
+
+  def parse(input: String) = input.linesIterator
+    .map { case s"$result: $operands" => Equation(result.toLong, operands.split(" ").map(_.toLong).toSeq) }
+    .toSeq
+
+  def check(equation: Equation, ops: Seq[(Long, Long) => Long]): Boolean =
+    def doCheck(target: Long, operands: Seq[Long], soFar: Long): Boolean =
+      if operands.isEmpty then soFar == target
+      else ops.exists(f => doCheck(target, operands.tail, f(soFar, operands.head)))
+    doCheck(equation.result, equation.operands.tail, equation.operands.head)
+
+  def part1(input: String) =
+    val equations = parse(input)
+    val ops: Seq[(Long, Long) => Long] = Seq(_ + _, _ * _)
+    equations.filter(check(_, ops)).map(_.result).sum
+
+  def part2(input: String) =
+    val equations = parse(input)
+    val ops: Seq[(Long, Long) => Long] = Seq(_ + _, _ * _, (a, b) => s"$a$b".toLong)
+    equations.filter(check(_, ops)).map(_.result).sum
---
+++ src/test/scala/org/lemon/advent/year2024/Day07Test.scala
@@ -0,0 +1,40 @@
+package org.lemon.advent.year2024
+
+import org.lemon.advent._
+import org.lemon.advent.year2024.Day07._
+
+class Day07Test extends UnitTest:
+
+  test("part 1 example") {
+    val in = """|190: 10 19
+                |3267: 81 40 27
+                |83: 17 5
+                |156: 15 6
+                |7290: 6 8 6 15
+                |161011: 16 10 13
+                |192: 17 8 14
+                |21037: 9 7 18 13
+                |292: 11 6 16 20""".stripMargin
+    part1(in) shouldBe 3749
+  }
+
+  test("part 1") {
+    part1(read(file(2024)(7))) shouldBe 5030892084481L
+  }
+
+  test("part 2 example") {
+    val in = """|190: 10 19
+                |3267: 81 40 27
+                |83: 17 5
+                |156: 15 6
+                |7290: 6 8 6 15
+                |161011: 16 10 13
+                |192: 17 8 14
+                |21037: 9 7 18 13
+                |292: 11 6 16 20""".stripMargin
+    part2(in) shouldBe 11387
+  }
+
+  test("part 2") {
+    part2(read(file(2024)(7))) shouldBe 91377448644679L
+  }
