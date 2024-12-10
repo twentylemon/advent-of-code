52821a8d94444cc53a248501c967e79bb11cb5de
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2024-12-10 10:18:48 -0500

    🎨 move `enumerate` to be `allPaths` library func

--- src/main/scala/org/lemon/advent/lib/graph/fill.scala
+++ src/main/scala/org/lemon/advent/lib/graph/fill.scala
@@ -17,3 +17,23 @@ def fill[N](adjacency: N => Seq[N], start: N): Set[N] =
     val node = queue.dequeue
     queue ++= adjacency(node).filter(nodes.add)
   nodes.toSet
+
+/** Performs a breadth first fill of the graph from the starting node to the ending nodes, returning
+  * all possible paths between the two sets.
+  *
+  * Note that if `adjacency` can form loops, this function will not terminate.
+  *
+  * @param adjacency function to return edges for a given node
+  * @param start the start node
+  * @param ends all possible ending nodes
+  * @return the set of all paths from `start` to any of `ends`
+  * @tparam N the node type
+  */
+def allPaths[N](adjacency: N => Seq[N], start: N, ends: Set[N]): Set[Path[N, Int]] =
+  val paths = mutable.Set.empty[Path[N, Int]]
+  val queue = mutable.Queue(Path(path = Seq(start), at = start, distance = 0))
+  while queue.nonEmpty do
+    val node @ Path(path, at, distance) = queue.dequeue
+    if ends(at) then paths.add(node)
+    queue ++= adjacency(at).map(neigh => Path(neigh +: path, neigh, distance + 1))
+  paths.toSet
--- src/main/scala/org/lemon/advent/year2024/Day10.scala
+++ src/main/scala/org/lemon/advent/year2024/Day10.scala
@@ -17,20 +17,11 @@ private object Day10:
       .map((c, _) => fill(adjacency, c).count(grid(_) == '9'))
       .sum
 
-  def enumerate(adjacency: Map[Coord, Seq[Coord]], start: Coord, ends: Set[Coord]) =
-    val queue = mutable.Queue(start)
-    var count = 0
-    while queue.nonEmpty do
-      val node = queue.dequeue
-      if ends(node) then count += 1
-      queue ++= adjacency(node)
-    count
-
   def part2(input: String) =
     val grid = parse(input)
     val adjacency = grid.map((c, h) => c -> c.adjacent.filter(grid.contains).filter(grid(_) == h + 1))
     val ends = grid.filter(_._2 == '9').keySet
     grid
       .filter(_._2 == '0')
-      .map((c, _) => enumerate(adjacency, c, ends))
+      .map((c, _) => allPaths(adjacency, c, ends).size)
       .sum
