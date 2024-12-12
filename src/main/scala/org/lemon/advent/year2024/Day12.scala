5da9c2c5c1cfc1e080d23af43c9e9d257133cc6c
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2024-12-12 11:06:17 -0500

    ✨ day 12 part 2

--- src/main/scala/org/lemon/advent/year2024/Day12.scala
+++ src/main/scala/org/lemon/advent/year2024/Day12.scala
@@ -7,18 +7,18 @@ import org.lemon.advent.lib.graph._
 
 private object Day12:
 
-  case class Plot(plot: Char, area: Set[Coord], perimeter: Set[Coord])
+  case class Plot(area: Set[Coord], perimeter: Set[Coord])
 
   def parse = Coord.gridToMap
 
   def adjacency(grid: Map[Coord, Char], start: Coord) =
     (c: Coord) => c.adjacent.filter(grid.contains).filter(grid(_) == grid(start))
 
-  def price(grid: Map[Coord, Char], start: Coord) =
-    val plot = grid(start)
+  def plot(grid: Map[Coord, Char], start: Coord) =
     val area = fill(adjacency(grid, start), start)
-    val perimeter = area.filter(_.adjacent.exists(!area.contains(_)))
-    Plot(plot, area, perimeter)
+    val perimeter = area.filter(_.surrounding.exists(!area.contains(_)))
+    // perimeter includes "inner corners" which are diagonal to the outside
+    Plot(area, perimeter)
 
   def perimeterLength(plot: Plot) =
     plot.perimeter.toSeq.map(_.adjacent.count(!plot.area.contains(_))).sum
@@ -26,12 +26,40 @@ private object Day12:
   def part1(input: String) =
     val grid = parse(input)
     val seen = mutable.Set.empty[Coord]
-    grid.keys.toSeq.view
+    grid.keys.iterator
       .filter(!seen.contains(_))
-      .map(price(grid, _))
+      .map(plot(grid, _))
       .tapEach(p => seen.addAll(p.area))
       .map(plot => plot.area.size * perimeterLength(plot))
       .sum
 
+  def numberOfSides(plot: Plot) =
+    def in(coord: Coord) = plot.area(coord)
+    def out(coord: Coord) = !plot.area(coord)
+    def sides(coord: Coord) =
+      // coord is adjacent to the outside, each contributes 1 side
+      val topLeft = out(coord.up) && out(coord.left)
+      val topRight = out(coord.up) && out(coord.right)
+      val botLeft = out(coord.down) && out(coord.left)
+      val botRight = out(coord.down) && out(coord.right)
+
+      // coord is diagonal to the outside, the shape has turned a corner
+      val holeUpLeft = in(coord.up) && in(coord.left) && out(coord.up.left)
+      val holeUpRight = in(coord.up) && in(coord.right) && out(coord.up.right)
+      val holeBotLeft = in(coord.down) && in(coord.left) && out(coord.down.left)
+      val holeBotRight = in(coord.down) && in(coord.right) && out(coord.down.right)
+
+      Seq(topLeft, topRight, botLeft, botRight, holeUpLeft, holeUpRight, holeBotLeft, holeBotRight)
+        .count(identity)
+
+    plot.perimeter.toSeq.map(sides).sum
+
   def part2(input: String) =
-    0
+    val grid = parse(input)
+    val seen = mutable.Set.empty[Coord]
+    grid.keys.iterator
+      .filter(!seen.contains(_))
+      .map(plot(grid, _))
+      .tapEach(p => seen.addAll(p.area))
+      .map(plot => plot.area.size * numberOfSides(plot))
+      .sum
--- src/test/scala/org/lemon/advent/year2024/Day12Test.scala
+++ src/test/scala/org/lemon/advent/year2024/Day12Test.scala
@@ -23,6 +23,25 @@ class Day12Test extends UnitTest:
     part1(read(file(2024)(12))) shouldBe 1486324
   }
 
+  test("part 2 E") {
+    val in = """|EEEEE
+                |EXXXX
+                |EEEEE
+                |EXXXX
+                |EEEEE""".stripMargin
+    part2(in) shouldBe 236
+  }
+
+  test("part 2 hole") {
+    val in = """|AAAAAA
+                |AAABBA
+                |AAABBA
+                |ABBAAA
+                |ABBAAA
+                |AAAAAA""".stripMargin
+    part2(in) shouldBe 368
+  }
+
   test("part 2 example") {
     val in = """|RRRRIICCFF
                 |RRRRIICCCF
@@ -34,9 +53,9 @@ class Day12Test extends UnitTest:
                 |MIIIIIJJEE
                 |MIIISIJEEE
                 |MMMISSJEEE""".stripMargin
-    part2(in) shouldBe 0
+    part2(in) shouldBe 1206
   }
 
   test("part 2") {
-    part2(read(file(2024)(12))) shouldBe 0
+    part2(read(file(2024)(12))) shouldBe 898684
   }
