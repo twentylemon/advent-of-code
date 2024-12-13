6b24d38ff9771bed6f4ddccf389de17eda3d0af4
twentylemon <taras.mychaskiw@gmail.com>
2024-12-12 23:57:22 -0500

    ✨ add `Range` extensions

--- src/main/scala/org/lemon/advent/lib/range.scala
+++ src/main/scala/org/lemon/advent/lib/range.scala
@@ -4,3 +4,16 @@ extension (range: Range)
   def intersects(rhs: Range): Boolean = range.min <= rhs.max && rhs.min <= range.max
 
   def intersect(rhs: Range): Range = math.max(range.min, rhs.min) to math.min(range.max, rhs.max)
+
+  def union(rhs: Range): Seq[Range] =
+    if range.intersects(rhs) then Seq(range.min.min(rhs.min) to range.max.max(rhs.max))
+    else Seq(range, rhs)
+
+  def diff(rhs: Range): Seq[Range] = ???
+
+extension (range: Range.type)
+  def unapply(str: String) =
+    str match
+      case s"$start-$end" => start.toIntOption.zip(end.toIntOption).map(_ to _)
+      case s"$start..$end" => start.toIntOption.zip(end.toIntOption).map(_ to _)
+      case _ => None
