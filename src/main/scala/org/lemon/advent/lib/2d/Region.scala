192c0f89228e335d8b4784fe389c68b9d2e4a6bd
twentylemon <taras.mychaskiw@gmail.com>
2024-12-12 23:34:08 -0500

    🚚 move day 12 solution to `Region` type

---
+++ src/main/scala/org/lemon/advent/lib/2d/Region.scala
@@ -0,0 +1,41 @@
+package org.lemon.advent.lib.`2d`
+
+object Region:
+  given Conversion[Region, Iterator[Coord]] = _.coords.iterator
+  given Conversion[Region, Set[Coord]] = _.coords
+
+case class Region(coords: Set[Coord]):
+  def area = coords.size
+
+  /** The perimeter of a region is the set of all coords that are on the boarder of the region.
+    * Coords which are diagonally adjacent are included in the perimeter, such that it forms
+    * a "continuous" line around the region.
+    *
+    * @return set of coords that are one away from the region
+    */
+  def perimeter: Set[Coord] = coords.filter(_.surrounding.exists(!coords.contains(_)))
+
+  def perimeterLength = perimeter.toSeq.map(_.adjacent.count(!coords.contains(_))).sum
+
+  def sides =
+    def in(coord: Coord) = coords(coord)
+    def out(coord: Coord) = !coords(coord)
+    def sides(coord: Coord) =
+      // coord is adjacent to the outside, each contributes 1 side
+      val topLeft = out(coord.up) && out(coord.left)
+      val topRight = out(coord.up) && out(coord.right)
+      val botLeft = out(coord.down) && out(coord.left)
+      val botRight = out(coord.down) && out(coord.right)
+
+      // coord is diagonal to the outside, the shape has turned a corner
+      val diagUpLeft = in(coord.up) && in(coord.left) && out(coord.up.left)
+      val diagUpRight = in(coord.up) && in(coord.right) && out(coord.up.right)
+      val diagBotLeft = in(coord.down) && in(coord.left) && out(coord.down.left)
+      val diagBotRight = in(coord.down) && in(coord.right) && out(coord.down.right)
+
+      Seq(topLeft, topRight, botLeft, botRight, diagUpLeft, diagUpRight, diagBotLeft, diagBotRight)
+        .count(identity)
+
+    perimeter.toSeq.map(sides).sum
+
+  def enclosing = Area(coords)
--- src/main/scala/org/lemon/advent/year2024/Day12.scala
+++ src/main/scala/org/lemon/advent/year2024/Day12.scala
@@ -14,52 +14,25 @@ private object Day12:
   def adjacency(grid: Map[Coord, Char], start: Coord) =
     (c: Coord) => c.adjacent.filter(grid.contains).filter(grid(_) == grid(start))
 
-  def plot(grid: Map[Coord, Char], start: Coord) =
-    val area = fill(adjacency(grid, start), start)
-    val perimeter = area.filter(_.surrounding.exists(!area.contains(_)))
-    // perimeter includes "inner corners" which are diagonal to the outside
-    Plot(area, perimeter)
-
-  def perimeterLength(plot: Plot) =
-    plot.perimeter.toSeq.map(_.adjacent.count(!plot.area.contains(_))).sum
+  def region(grid: Map[Coord, Char], start: Coord) =
+    Region(fill(adjacency(grid, start), start))
 
   def part1(input: String) =
     val grid = parse(input)
     val seen = mutable.Set.empty[Coord]
     grid.keys.iterator
       .filter(!seen.contains(_))
-      .map(plot(grid, _))
-      .tapEach(p => seen.addAll(p.area))
-      .map(plot => plot.area.size * perimeterLength(plot))
+      .map(region(grid, _))
+      .tapEach(region => seen.addAll(region.coords))
+      .map(region => region.area * region.perimeterLength)
       .sum
-
-  def numberOfSides(plot: Plot) =
-    def in(coord: Coord) = plot.area(coord)
-    def out(coord: Coord) = !plot.area(coord)
-    def sides(coord: Coord) =
-      // coord is adjacent to the outside, each contributes 1 side
-      val topLeft = out(coord.up) && out(coord.left)
-      val topRight = out(coord.up) && out(coord.right)
-      val botLeft = out(coord.down) && out(coord.left)
-      val botRight = out(coord.down) && out(coord.right)
-
-      // coord is diagonal to the outside, the shape has turned a corner
-      val holeUpLeft = in(coord.up) && in(coord.left) && out(coord.up.left)
-      val holeUpRight = in(coord.up) && in(coord.right) && out(coord.up.right)
-      val holeBotLeft = in(coord.down) && in(coord.left) && out(coord.down.left)
-      val holeBotRight = in(coord.down) && in(coord.right) && out(coord.down.right)
-
-      Seq(topLeft, topRight, botLeft, botRight, holeUpLeft, holeUpRight, holeBotLeft, holeBotRight)
-        .count(identity)
-
-    plot.perimeter.toSeq.map(sides).sum
 
   def part2(input: String) =
     val grid = parse(input)
     val seen = mutable.Set.empty[Coord]
     grid.keys.iterator
       .filter(!seen.contains(_))
-      .map(plot(grid, _))
-      .tapEach(p => seen.addAll(p.area))
-      .map(plot => plot.area.size * numberOfSides(plot))
+      .map(region(grid, _))
+      .tapEach(region => seen.addAll(region.coords))
+      .map(region => region.area * region.sides)
       .sum
