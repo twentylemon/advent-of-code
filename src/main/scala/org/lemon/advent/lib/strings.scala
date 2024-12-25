159962f479f7e9e12aeade49a8d841ea10b4e2ce
twentylemon <taras.mychaskiw@gmail.com>
2024-12-24 23:50:22 -0500

    🎨 docs

--- src/main/scala/org/lemon/advent/lib/strings.scala
+++ src/main/scala/org/lemon/advent/lib/strings.scala
@@ -1,4 +1,10 @@
 package org.lemon.advent.lib
 
 extension (str: String)
-  def padLeft(n: Int, c: Char) = c.toString * (n - str.length) + str
+  /** Prepends the string with the given character until it reaches the given length.
+    *
+    * @param len the desired length of the string
+    * @param c the character to prepend
+    * @return the string padded to the left with the given character
+    */
+  def padLeft(len: Int, c: Char) = c.toString * (len - str.length) + str
