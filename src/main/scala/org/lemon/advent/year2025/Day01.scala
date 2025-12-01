9a9c7706fbe67d3728e0aea6162471e6c107c2c0
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2025-12-01 11:02:19 -0500

    🎨 clean up day 1 (#76)

--- src/main/scala/org/lemon/advent/year2025/Day01.scala
+++ src/main/scala/org/lemon/advent/year2025/Day01.scala
@@ -1,8 +1,5 @@
 package org.lemon.advent.year2025
 
-import org.lemon.advent.lib._
-import scala.collection.mutable
-
 private object Day01:
 
   def parse(input: String) =
@@ -14,19 +11,15 @@ private object Day01:
 
   def part1(input: String) =
     val turns = parse(input)
-    var twists = Seq(50)
-    turns.foreach(x =>
-      val at = (twists.last + x) +% 100
-      twists = twists ++ Seq(at)
-    )
-    twists.count(_ == 0)
+    turns.scanLeft(50)(_ + _).count(_ % 100 == 0)
 
   def part2(input: String) =
     val turns = parse(input)
-    var (pastZero, at) = (0, 50)
-    turns.foreach(x =>
-      val afterTurn = at + x
-      pastZero += (afterTurn / 100).abs + (if at.signum == 0 || at.signum == afterTurn.signum then 0 else 1)
-      at = afterTurn +% 100
-    )
-    pastZero
+    turns.scanLeft(50)(_ + _).sliding(2).map {
+      case Seq(from, to) if from < to => Math.floorDiv(to, 100) - Math.floorDiv(from, 100)
+      case Seq(from, to) if from > to =>
+        val minCentury = Math.floorDiv(to, 100) + (if (to % 100 == 0) 0 else 1)
+        val maxCentury = Math.floorDiv(from - 1, 100)
+        (maxCentury - minCentury + 1).max(0)
+      case _ => 0
+    }.sum
