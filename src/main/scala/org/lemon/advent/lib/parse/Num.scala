8c32f25b385309d2c381d50a1a1735bb47d175a1
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2025-12-04 15:28:34 -0500

    :fire: remove unused numeric extractors

--- src/main/scala/org/lemon/advent/lib/parse/Num.scala
+++
@@ -1,6 +0,0 @@
-package org.lemon.advent.lib.parse
-
-/** Generic extractor object for parsing numbers.
-  */
-object Num:
-  def unapply[T: Numeric](s: String): Option[T] = Numeric[T].parseString(s)
