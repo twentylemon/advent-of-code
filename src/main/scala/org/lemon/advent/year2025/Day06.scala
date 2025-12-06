b6cbdc3270e9713045ba92bd4e7b2fbca14dc411
twentylemon <taras.mychaskiw@gmail.com>
2025-12-06 00:37:48 -0500

    :sparkle: day 6

---
+++ src/main/scala/org/lemon/advent/year2025/Day06.scala
@@ -0,0 +1,45 @@
+package org.lemon.advent.year2025
+
+import org.lemon.advent.lib._
+import scala.annotation.tailrec
+
+private object Day06:
+
+  def parse(input: String) =
+    import org.lemon.advent.lib.parse._
+    val lines = input.linesIterator.toSeq
+    val operands = lines.init.map(_ match
+      case Wsv(xs*) => xs.map(_.toLong)
+    )
+    val operators =
+      lines.last match
+        case Wsv(ops*) => ops
+
+    (operands, operators)
+
+  def part1(input: String) =
+    val (operands, operators) = parse(input)
+    operators.zip(operands.transpose)
+      .map((op, xs) =>
+        op match
+          case "+" => xs.sum
+          case "*" => xs.product
+      )
+      .sum
+
+  def split[A](seq: Seq[A])(value: A): Seq[Seq[A]] =
+    if seq.isEmpty then Seq.empty
+    else
+      val (prefix, rest) = seq.span(_ != value)
+      prefix +: split(rest.drop(1))(value)
+
+  def part2(input: String) =
+    val lines = input.linesIterator.toSeq
+    val groups = split(lines.transpose)(List.fill(lines.size)(' '))
+    groups.map(problem =>
+      val op = problem.head.last
+      val operands = problem.map(_.init.mkString.trim.toLong)
+      op match
+        case '+' => operands.sum
+        case '*' => operands.product
+    ).sum
---
+++ src/test/scala/org/lemon/advent/year2025/Day06Test.scala
@@ -0,0 +1,32 @@
+package org.lemon.advent.year2025
+
+import org.lemon.advent._
+import org.lemon.advent.year2025.Day06._
+
+class Day06Test extends UnitTest:
+
+  test("part 1 example") {
+    val in = """|123 328  51 64 
+                | 45 64  387 23 
+                |  6 98  215 314
+                |*   +   *   +  
+                |""".stripMargin
+    part1(in) shouldBe 4277556
+  }
+
+  test("part 1") {
+    part1(read(file(2025)(6))) shouldBe 3525371263915L
+  }
+
+  test("part 2 example") {
+    val in = """|123 328  51 64 
+                | 45 64  387 23 
+                |  6 98  215 314
+                |*   +   *   +  
+                |""".stripMargin
+    part2(in) shouldBe 3263827
+  }
+
+  test("part 2") {
+    part2(read(file(2025)(6))) shouldBe 6846480843636L
+  }
