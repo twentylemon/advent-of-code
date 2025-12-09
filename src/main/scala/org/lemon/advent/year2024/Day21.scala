ac7505b6771d166f586a5e0862880d8f89fa5b87
twentylemon <taras.mychaskiw@gmail.com>
2025-12-08 22:17:00 -0500

    fmt

--- src/main/scala/org/lemon/advent/year2024/Day21.scala
+++ src/main/scala/org/lemon/advent/year2024/Day21.scala
@@ -69,69 +69,70 @@ private object Day21:
       .map(code => complexity(code, pathLength(code, 25)))
       .sum
 
-  // case class Node(
-  //     depressurized: Robit, // at the keypad
-  //     radiated: Robit, // at the directional pad controlling depressurized
-  //     cold: Robit, // at the directional pad controlling radiated
-  //     pressed: String = "",
-  // ):
-  //   override def toString(): String = s"Node(d=$depressurized, r=$radiated, c=$cold, $pressed)"
-  // case class Robit(armLocation: Coord, buttons: Seq[Seq[Char]], pushed: String = ""):
-  //   def pointingAt = buttons(armLocation)
-  //   def wouldBePointingAt(coord: Coord) = Option.when(buttons.hasCoord(coord))(buttons(coord))
-  //   override def toString(): String = s"Robit($pointingAt, $pushed)"
+  // GARBO
+  case class Node(
+      depressurized: Robit, // at the keypad
+      radiated: Robit, // at the directional pad controlling depressurized
+      cold: Robit, // at the directional pad controlling radiated
+      pressed: String = "",
+  ):
+    override def toString(): String = s"Node(d=$depressurized, r=$radiated, c=$cold, $pressed)"
+  case class Robit(armLocation: Coord, buttons: Seq[Seq[Char]], pushed: String = ""):
+    def pointingAt = buttons(armLocation)
+    def wouldBePointingAt(coord: Coord) = Option.when(buttons.hasCoord(coord))(buttons(coord))
+    override def toString(): String = s"Robit($pointingAt, $pushed)"
 
-  // def adjacency(code: String)(node: Node): Seq[Node] =
-  //   val Node(depressurized, radiated, cold, _) = node
-  //   // we can push any of the direction buttons, which will move cold's arm
-  //   val directionalButtons = Direction.values
-  //     .map(cold.armLocation + _)
-  //     .filter(cold.wouldBePointingAt(_).exists(_ != ' '))
-  //     .map(newLoc =>
-  //       node.copy(
-  //         cold = cold.copy(armLocation = newLoc),
-  //         pressed =
-  //           cold.armLocation.directionTo(newLoc).get match
-  //             case Direction.Up => "^"
-  //             case Direction.Down => "v"
-  //             case Direction.Left => "<"
-  //             case Direction.Right => ">"
-  //       )
-  //     )
-  //     .toSeq
+  def adjacency(code: String)(node: Node): Seq[Node] =
+    val Node(depressurized, radiated, cold, _) = node
+    // we can push any of the direction buttons, which will move cold's arm
+    val directionalButtons = Direction.values
+      .map(cold.armLocation + _)
+      .filter(cold.wouldBePointingAt(_).exists(_ != ' '))
+      .map(newLoc =>
+        node.copy(
+          cold = cold.copy(armLocation = newLoc),
+          pressed =
+            cold.armLocation.directionTo(newLoc).get match
+              case Direction.Up => "^"
+              case Direction.Down => "v"
+              case Direction.Left => "<"
+              case Direction.Right => ">"
+        )
+      )
+      .toSeq
 
-  //   // we can push A, which will push cold's active button
-  //   val a = cold.pointingAt match
-  //     case ' ' => None // pointing at a gap, cannot be pushed
-  //     case 'A' => // if cold is gonna push A, radiated will push it's button
-  //       radiated.pointingAt match
-  //         case ' ' => None // pointing at a gap, cannot be pushed
-  //         case 'A' => // radiated is pushing A, so depressurized will push it's button
-  //           depressurized.pointingAt match
-  //             case ' ' => None // pointing at a gap, cannot be pushed
-  //             case key => // depressurized is pushing a button, so we can push it
-  //               val newPushed = depressurized.pushed + key
-  //               Option.when(code.startsWith(newPushed))(
-  //                 node.copy(depressurized = depressurized.copy(pushed = newPushed), pressed = "A"),
-  //               )
-  //         case radiatedDir => // radiated is pointing at a direction, so it will move depressurized's arm
-  //           val newLoc = depressurized.armLocation + Direction(radiatedDir)
-  //           depressurized.wouldBePointingAt(newLoc).filter(_ != ' ')
-  //             .map(_ =>
-  //               node.copy(depressurized = depressurized.copy(armLocation = newLoc), pressed = "A")
-  //             )
-  //     case coldDir => // cold is pointing at a direction, so it will move radiated's arm
-  //       val newLoc = radiated.armLocation + Direction(coldDir)
-  //       radiated.wouldBePointingAt(newLoc).filter(_ != ' ')
-  //         .map(_ =>
-  //           node.copy(radiated = radiated.copy(armLocation = newLoc), pressed = "A")
-  //         )
-  //   directionalButtons ++ a.toSeq
+    // we can push A, which will push cold's active button
+    val a = cold.pointingAt match
+      case ' ' => None // pointing at a gap, cannot be pushed
+      case 'A' => // if cold is gonna push A, radiated will push it's button
+        radiated.pointingAt match
+          case ' ' => None // pointing at a gap, cannot be pushed
+          case 'A' => // radiated is pushing A, so depressurized will push it's button
+            depressurized.pointingAt match
+              case ' ' => None // pointing at a gap, cannot be pushed
+              case key => // depressurized is pushing a button, so we can push it
+                val newPushed = depressurized.pushed + key
+                Option.when(code.startsWith(newPushed))(
+                  node.copy(depressurized = depressurized.copy(pushed = newPushed), pressed = "A"),
+                )
+          case radiatedDir => // radiated is pointing at a direction, so it will move depressurized's arm
+            val newLoc = depressurized.armLocation + Direction(radiatedDir)
+            depressurized.wouldBePointingAt(newLoc).filter(_ != ' ')
+              .map(_ =>
+                node.copy(depressurized = depressurized.copy(armLocation = newLoc), pressed = "A")
+              )
+      case coldDir => // cold is pointing at a direction, so it will move radiated's arm
+        val newLoc = radiated.armLocation + Direction(coldDir)
+        radiated.wouldBePointingAt(newLoc).filter(_ != ' ')
+          .map(_ =>
+            node.copy(radiated = radiated.copy(armLocation = newLoc), pressed = "A")
+          )
+    directionalButtons ++ a.toSeq
 
-  // def route(code: String) =
-  //   val start = Node(
-  //     depressurized = Robit(Coord(2, 3), keypad),
-  //     radiated = Robit(Coord(2, 0), directional),
-  //     cold = Robit(Coord(2, 0), directional),
-  //   )
-  //   pathFind(adjacency(code), start, _.depressurized.pushed == code)
+  def route(code: String) =
+    val start = Node(
+      depressurized = Robit(Coord(2, 3), keypad),
+      radiated = Robit(Coord(2, 0), directional),
+      cold = Robit(Coord(2, 0), directional),
+    )
+    pathFind(adjacency(code), start, _.depressurized.pushed == code)
