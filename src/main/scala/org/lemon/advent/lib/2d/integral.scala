013add174dcd232726846d4c5c474fc7abd83bb0
Taras Mychaskiw <twentylemon@users.noreply.github.com>
2025-12-09 22:54:04 -0500

    :art: use integral utils (#84)

--- src/main/scala/org/lemon/advent/lib/2d/integral.scala
+++
@@ -1,8 +0,0 @@
-package org.lemon.advent.lib.`2d`
-
-import scala.math.Integral.Implicits.*
-
-private def `0`[N: Integral]: N = Integral[N].zero
-private def `1`[N: Integral]: N = Integral[N].one
-private def `-1`[N: Integral]: N = -Integral[N].one
-private def fromInt[N: Integral](n: Int): N = Integral[N].fromInt(n)
