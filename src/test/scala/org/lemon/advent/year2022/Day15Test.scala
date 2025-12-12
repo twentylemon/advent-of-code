95d3957f231af4310c021b257c944fd60089c176
twentylemon <taras.mychaskiw@gmail.com>
2025-12-12 18:59:52 -0500

    day 15

--- src/test/scala/org/lemon/advent/year2022/Day15Test.scala
+++ src/test/scala/org/lemon/advent/year2022/Day15Test.scala
@@ -1,41 +1,9 @@
 package org.lemon.advent.year2022
 
 import org.lemon.advent.*
-import org.lemon.advent.lib.*
+import org.lemon.advent.year2022.Day15.*
 
-class Day15Test extends UnitTest {
-
-  type Coord = (Int, Int)
-  extension (coord: Coord)
-    def x = coord._1
-    def y = coord._2
-    def manhattan(rhs: Coord) = (coord.x - rhs.x).abs + (coord.y - rhs.y).abs
-
-  case class Sensor(sensor: Coord, beacon: Coord):
-    val distance = sensor `manhattan` beacon
-
-  def parseSensor(line: String) = line match
-    case s"Sensor at x=$xs, y=$ys: closest beacon is at x=$xb, y=$yb" =>
-      Sensor(sensor = (xs.toInt, ys.toInt), beacon = (xb.toInt, yb.toInt))
-
-  def coverage(sensor: Sensor, row: Int) =
-    val diff = (sensor.sensor.y - row).abs
-    if diff <= sensor.distance then
-      (sensor.sensor.x - sensor.distance + diff) to (sensor.sensor.x + sensor.distance - diff)
-    else 0 until 0
-
-  def coverageTree(sensors: Seq[Sensor], row: Int) =
-    sensors.foldLeft(Diet.empty[Int])((diet, sensor) => diet + coverage(sensor, row))
-
-  def part1(in: Seq[String], row: Int) =
-    val sensors = in.map(parseSensor)
-    val diet = coverageTree(sensors, row)
-    diet.intervalsIterator.map((start, end) => end - start).sum
-
-  def part2(in: Seq[String], range: Range): Long =
-    val sensors = in.map(parseSensor)
-    val (diet, row) = range.iterator.map(coverageTree(sensors, _)).zipWithIndex.dropWhile(_._1(range)).next
-    (diet.toIntervals.head.end + 1) * 4000000L + row
+class Day15Test extends UnitTest:
 
   test("part 1 example") {
     val in = """|Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -82,5 +50,3 @@ class Day15Test extends UnitTest {
   test("part 2") {
     part2(readLines(file(2022)(15)), 0 to 4000000) shouldBe 12543202766584L
   }
-
-}
