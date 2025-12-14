a5900dc5974ef54c50fdd4d565b09d8592ad90bc
twentylemon <taras.mychaskiw@gmail.com>
2025-12-13 19:10:49 -0500

    :art: use `Coord` and `Area`

--- src/main/scala/org/lemon/advent/year2022/Day14.scala
+++ src/main/scala/org/lemon/advent/year2022/Day14.scala
@@ -1,36 +1,25 @@
 package org.lemon.advent.year2022
 
+import org.lemon.advent.lib.`2d`.*
+
 import scala.collection.mutable
 
 private object Day14:
-
-  type Coord = (Int, Int)
-  extension (coord: Coord)
-    def x = coord._1
-    def y = coord._2
-    def up = (coord.x, coord.y - 1)
-    def down = (coord.x, coord.y + 1)
-    def left = (coord.x - 1, coord.y)
-    def right = (coord.x + 1, coord.y)
-
-  type Rock = (Range, Range)
-  extension (rock: Rock)
-    def xs = rock._1
-    def ys = rock._2
 
   enum Trickle:
     case Rest(coord: Coord)
     case Fall
   import Trickle.*
 
-  def parseRocks(line: String): Iterator[Rock] = line
+  def parseRocks(line: String): Iterator[Area] = line
     .split(" -> ")
-    .map({ case s"$x,$y" => (x.toInt, y.toInt) })
+    .toSeq
+    .map { case s"$x,$y" => Coord(x.toInt, y.toInt) }
     .sliding(2)
-    .map(pair => (pair.map(_.x).min to pair.map(_.x).max, pair.map(_.y).min to pair.map(_.y).max))
+    .map { case Seq(c1, c2) => c1.bounding(c2) }
 
   def trickle(occupied: Coord => Boolean, deep: Coord => Boolean)(source: Coord): Trickle =
-    val spot = Iterator.iterate(source)(c => c.down)
+    val spot = source.walk(Direction.Down)
       .dropWhile(c => !deep(c) && !occupied(c))
       .next
 
@@ -39,33 +28,33 @@ private object Day14:
     else if !occupied(spot.right) then trickle(occupied, deep)(spot.right)
     else Rest(spot.up)
 
-  def part1(input: Seq[String]): Int =
-    val rocks = input.flatMap(parseRocks)
-    val deepest = rocks.map(_.ys.end).max
-    val initialBlocks = rocks.flatMap(r => for x <- r.xs; y <- r.ys yield (x, y)).toSet
+  def part1(input: String): Int =
+    val rocks = input.linesIterator.flatMap(parseRocks).toSeq
+    val deepest = rocks.map(_.yRange.end).max
+    val initialBlocks = rocks.flatten.toSet
     val sand = mutable.Set.from(initialBlocks)
     def occupied = sand.contains
     def deep(coord: Coord) = coord.y > deepest
 
     var resting = true
     while resting do
-      trickle(occupied, deep)((500, 0)) match
+      trickle(occupied, deep)(Coord(500, 0)) match
         case Fall => resting = false
         case Rest(coord) => sand += coord
     sand.size - initialBlocks.size
 
-  def part2(input: Seq[String]): Int =
-    val rocks = input.flatMap(parseRocks)
-    val deepest = rocks.map(_.ys.end).max
-    val initialBlocks = rocks.flatMap(r => for x <- r.xs; y <- r.ys yield (x, y)).toSet
+  def part2(input: String): Int =
+    val rocks = input.linesIterator.flatMap(parseRocks).toSeq
+    val deepest = rocks.map(_.yRange.end).max
+    val initialBlocks = rocks.flatten.toSet
     val sand = mutable.Set.from(initialBlocks)
     def occupied(coord: Coord) = coord.y >= deepest + 2 || sand.contains(coord)
     def deep(coord: Coord) = false
 
     var resting = true
     while resting do
-      trickle(occupied, deep)((500, 0)) match
-        case Rest((500, 0)) => resting = false
+      trickle(occupied, deep)(Coord(500, 0)) match
+        case Rest(Coord(500, 0)) => resting = false
         case Rest(coord) => sand += coord
         case Fall => throw AssertionError()
     sand.size - initialBlocks.size + 1
--- src/test/scala/org/lemon/advent/year2022/Day14Test.scala
+++ src/test/scala/org/lemon/advent/year2022/Day14Test.scala
@@ -9,20 +9,20 @@ class Day14Test extends UnitTest:
     val in = """|498,4 -> 498,6 -> 496,6
                 |503,4 -> 502,4 -> 502,9 -> 494,9""".stripMargin
 
-    part1(in.linesIterator.toSeq) shouldBe 24
+    part1(in) shouldBe 24
   }
 
   test("part 1") {
-    part1(readLines(file(2022)(14))) shouldBe 728
+    part1(read(file(2022)(14))) shouldBe 728
   }
 
   test("part 2 example") {
     val in = """|498,4 -> 498,6 -> 496,6
                 |503,4 -> 502,4 -> 502,9 -> 494,9""".stripMargin
 
-    part2(in.linesIterator.toSeq) shouldBe 93
+    part2(in) shouldBe 93
   }
 
   test("part 2") {
-    part2(readLines(file(2022)(14))) shouldBe 27623
+    part2(read(file(2022)(14))) shouldBe 27623
   }
