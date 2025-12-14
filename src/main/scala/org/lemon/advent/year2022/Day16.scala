e6ca1cd08e2c0d616017991b3ee3b522c9dac698
twentylemon <taras.mychaskiw@gmail.com>
2025-12-14 18:00:30 -0500

    :art: use `memoize` and :zap: it up

--- src/main/scala/org/lemon/advent/year2022/Day16.scala
+++ src/main/scala/org/lemon/advent/year2022/Day16.scala
@@ -1,7 +1,7 @@
 package org.lemon.advent.year2022
 
 import org.lemon.advent.lib.*
-import scala.collection.mutable
+import org.lemon.advent.lib.graph.*
 import scala.collection.BitSet
 
 private object Day16:
@@ -12,45 +12,44 @@ private object Day16:
     case s"Valve $id has flow rate=$rate; $_ $_ to $_ $tunnels" =>
       Valve(id = id, rate = rate.toInt, tunnels = tunnels.csv)
 
-  def maxFlow(
-      currentValve: String,
-      time: Int,
-      openFlowValves: BitSet,
-      turn: Int
-  )(using
-      valves: Map[String, Valve],
-      flowValves: Seq[Valve],
-      memory: mutable.Map[String, Int] = mutable.Map()
-  ): Int =
-    val key = s"$turn#$currentValve#$time#${openFlowValves.toBitMask(0)}"
+  def maxFlow(startValve: String, timeLimit: Int, turnsRemaining: Int, allValves: Seq[Valve]): Int =
+    val valveByName = allValves.map(v => v.id -> v).toMap
+    val flowValves = allValves.filter(_.rate > 0)
+    val flowValveIndex = flowValves.zipWithIndex.toMap
 
-    memory.get(key) match
-      case Some(memorized) => memorized
-      case None =>
-        val flow =
-          if time <= 0 && turn > 0 then maxFlow("AA", 26, openFlowValves, turn - 1) // hard code restart for elebro. meh
-          else if time <= 0 && turn == 0 then 0
-          else
-            val valve = valves(currentValve)
-            val valveIndex = flowValves.indexOf(valve)
-            val moveOn = valve.tunnels.map(v => maxFlow(v, time - 1, openFlowValves, turn))
-            val open =
-              if valveIndex < 0 || openFlowValves(valveIndex) then 0
-              else (time - 1) * valve.rate + maxFlow(currentValve, time - 1, openFlowValves + valveIndex, turn)
+    val adjacency = (id: String) => valveByName(id).tunnels
+    val sources = startValve +: flowValves.map(_.id)
+    val distances = (for
+      from <- sources
+      to <- flowValves.map(_.id)
+      path <- pathFind(adjacency, from, to)
+    yield (from, to) -> path.distance).toMap
 
-            (moveOn ++ Seq(open)).max
+    lazy val compute: (String, Int, BitSet, Int) => Int = memoize { (valve, timeLeft, openValves, turnsLeft) =>
+      val best = flowValveIndex
+        .filterNot((_, idx) => openValves(idx))
+        .flatMap((target, idx) =>
+          distances.get((valve, target.id))
+            .filter(d => d + 1 < timeLeft)
+            .map(d =>
+              val remaining = timeLeft - d - 1
+              remaining * target.rate + compute(target.id, remaining, openValves + idx, turnsLeft)
+            )
+        )
+        .maxOption
+        .getOrElse(0)
 
-        memory += (key -> flow)
-        flow
+      // elebro takes over
+      if turnsLeft > 0 then best.max(compute(startValve, timeLimit, openValves, turnsLeft - 1))
+      else best
+    }
+
+    compute(startValve, timeLimit, BitSet.empty, turnsRemaining)
 
   def part1(input: String): Int =
     val allValves = input.linesIterator.map(parseValve).toSeq
-    given Seq[Valve] = allValves.filter(_.rate > 0)
-    given Map[String, Valve] = allValves.map(v => v.id -> v).toMap
-    maxFlow("AA", 30, BitSet.empty, 0)
+    maxFlow("AA", 30, 0, allValves)
 
   def part2(input: String): Int =
     val allValves = input.linesIterator.map(parseValve).toSeq
-    given Seq[Valve] = allValves.filter(_.rate > 0)
-    given Map[String, Valve] = allValves.map(v => v.id -> v).toMap
-    maxFlow("AA", 26, BitSet.empty, 1)
+    maxFlow("AA", 26, 1, allValves)
