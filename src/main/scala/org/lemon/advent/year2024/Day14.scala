c7f9ebe81cb9ab2fadea8b2605b9aee68fba23a9
Taras Mychaskiw <taras.mychaskiw@instacart.com>
2025-12-15 10:18:09 -0500

    :art: use `nth`

--- src/main/scala/org/lemon/advent/year2024/Day14.scala
+++ src/main/scala/org/lemon/advent/year2024/Day14.scala
@@ -1,5 +1,6 @@
 package org.lemon.advent.year2024
 
+import org.lemon.advent.lib.*
 import org.lemon.advent.lib.`2d`.*
 
 private object Day14:
@@ -22,13 +23,11 @@ private object Day14:
   def part1(input: String, example: Boolean) =
     val area = if example then Area(11, 7) else Area(101, 103)
     val robits = parse(input)
-    val later = Iterator.iterate(robits)(move(_, area)).drop(100).next
+    val later = Iterator.iterate(robits)(move(_, area)).nth(100)
     quads(later, area).product
 
   def part2(input: String) =
     val area = Area(101, 103)
     val robits = parse(input)
-    Iterator.iterate(robits)(move(_, area))
-      .drop(8168)
-      .map(robits => area.show(c => if robits.exists(_._1 == c) then '#' else '.'))
-      .next
+    val later = Iterator.iterate(robits)(move(_, area)).nth(8168)
+    area.show(c => if later.exists(_._1 == c) then '#' else '.')
