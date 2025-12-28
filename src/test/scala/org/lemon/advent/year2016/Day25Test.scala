abf2eece4562d438948469e6892aed2bfa67fb0a
twentylemon <taras.mychaskiw@gmail.com>
2025-12-28 13:17:28 -0500

    :sparkle: day 25 2016

---
+++ src/test/scala/org/lemon/advent/year2016/Day25Test.scala
@@ -0,0 +1,10 @@
+package org.lemon.advent.year2016
+
+import org.lemon.advent.*
+import org.lemon.advent.year2016.Day25.*
+
+class Day25Test extends UnitTest:
+
+  test("part 1") {
+    part1(read(file(2016)(25))) shouldBe 196
+  }
